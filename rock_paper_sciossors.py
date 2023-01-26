99b75c59fe6d28bf8abab263cd30468e2fedea58
DimaPopova <dima.popova9705@gmail.com>
2023-01-26 22:07:54 +0100

    Update rock_paper_sciossors.py

--- rock_paper_sciossors.py
+++ rock_paper_sciossors.py
@@ -13,16 +13,18 @@ game_mode = input("Enter '1' for first to win or enter '2' for first to two wins
 if game_mode == "1":
     print("First to win.")
     print("Start! Please chose rock,paper or scissors")
-    flag = False
+
     while True:
         player_one_choice = input()
         computer_choice = random.choice(choices)
+
         if (player_one_choice == "rock" and computer_choice == "scissors") or \
                 (player_one_choice == "paper" and computer_choice == "rock") or \
                 (player_one_choice == "scissors" and computer_choice == "paper"):
 
             print(f"The computer chose: {computer_choice}")
             print(f"{player_one} Wins!")
+
         elif player_one_choice == computer_choice:
             print(f"The computer chose: {computer_choice}")
             print("Draw")
@@ -30,6 +32,7 @@ if game_mode == "1":
         elif (computer_choice == "rock" and player_one_choice == "scissors") or \
                 (computer_choice == "paper" and player_one_choice == "rock") or \
                 (computer_choice == "scissors" and player_one_choice == "paper"):
+
             print(f"The computer chose: {computer_choice}")
             print(f"The {player_two} Wins!")
 
@@ -40,6 +43,7 @@ if game_mode == "1":
 
         print(f"Do you want one more game {player_one}?")
         answer = input()
+
         if answer == "no":
             print("Ok, bye!")
             exit()
@@ -52,15 +56,18 @@ if game_mode == "1":
             print("Incorrect answer!")
             print("You have to answer with 'yes' or 'no'.")
             answer = input()
+
 # 2 from 3
 elif game_mode == "2":
     print("2 from 3")
     print("Start! Please chose rock,paper or scissors")
     counter_player_one = 0
     counter_player_two = 0
+
     while counter_player_one < 2 and counter_player_two < 2:
         player_one_choice = input()
         computer_choice = random.choice(choices)
+
         if (player_one_choice == "rock" and computer_choice == "scissors") or \
                 (player_one_choice == "paper" and computer_choice == "rock") or \
                 (player_one_choice == "scissors" and computer_choice == "paper"):
@@ -68,6 +75,7 @@ elif game_mode == "2":
             counter_player_one += 1
             print(f"The computer chose: {computer_choice}")
             print(f"Result: {player_one}-{counter_player_one} : {player_two}-{counter_player_two}")
+
         elif player_one_choice == computer_choice:
             print(f"The computer chose: {computer_choice}")
             print("Draw")
@@ -75,6 +83,7 @@ elif game_mode == "2":
         elif (computer_choice == "rock" and player_one_choice == "scissors") or \
                 (computer_choice == "paper" and player_one_choice == "rock") or \
                 (computer_choice == "scissors" and player_one_choice == "paper"):
+
             counter_player_two += 1
             print(f"The computer chose: {computer_choice}")
             print(f"Result: {player_one}-{counter_player_one} : {player_two}-{counter_player_two}")
@@ -88,15 +97,18 @@ elif game_mode == "2":
             print(f"{player_one} Wins!")
             print(f"Do you want one more set {player_one}?")
             new_answer = input()
+
             if new_answer == "no":
                 print("Ok, bye!")
                 exit()
+
             elif new_answer == "yes":
                 counter_player_two = 0
                 counter_player_one = 0
                 print("OK, let's play.")
                 print("Start! Please chose rock,paper or scissors")
                 continue
+
         elif counter_player_two == 2 and counter_player_two > counter_player_one:
             print(f"The {player_two} Wins!")
             print(f"Do you want one more set {player_one}?")
@@ -105,6 +117,7 @@ elif game_mode == "2":
             if new_answer == "no":
                 print("Ok, bye!")
                 exit()
+
             elif new_answer == "yes":
                 counter_player_two = 0
                 counter_player_one = 0
